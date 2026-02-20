79e6f87403916b1a088b22be714cde393420961e
Sanjana M Patil <sanjanampatil6@gmail.com>
2026-02-20 23:47:30 +0530

    adde

--- frontend/src/Home/CandidateOverview.jsx
+++ frontend/src/Home/CandidateOverview.jsx
@@ -527,6 +527,7 @@ const CandidateOverview = () => {
                       }}
                     />
 
+
                     {/* Select/Reject Buttons */}
                     <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
                       <button
